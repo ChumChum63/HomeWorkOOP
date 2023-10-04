6668ad717722dfb43ded58ccd9f121e2cf627189
Vasya Nikolaev <nikovasia@gmail.com>
2023-10-04 18:05:40 +0300

    HomeWork_end

--- HomeWork #2.py
+++ HomeWork #2.py
@@ -16,6 +16,19 @@ class Student:
         else:
             return 'Ошибка'
 
+    def avg_score(self):
+        for num in self.grades.values():
+            return sum(num) / len(num)
+        if len(self.grades) == 0:
+            return f"Нет оценок"
+
+    def __str__(self):
+        return (f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.avg_score()}\n"
+                f"Курсы в процессе изучения: {','.join(self.courses_in_progress)}\nЗавершенные курсы: {','.join(self.finished_courses)}")
+
+
+    def __lt__(self, other):
+        return self.avg_score() < other.avg_score()
 
 class Mentor:
     def __init__(self, name, surname):
@@ -27,6 +40,18 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.lecture_grade = {}
+
+    def avg_score(self):
+        for num in self.lecture_grade.values():
+            return sum(num) / len(num)
+        if len(self.lecture_grade) == 0:
+            return f"Нет оценок"
+
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.avg_score()}"
+
+    def __lt__(self, other):
+        return self.avg_score() < other.avg_score()
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -41,23 +66,86 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
+
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Python', 'Git']
+best_student.finished_courses += ['Java']
+
+bad_student = Student('Lyffi', 'Monkey D', 'your_gender')
+bad_student.courses_in_progress += ['Java', 'Git']
+bad_student.finished_courses += ['Python']
 
 cool_reviewer = Reviewer('Some', 'Buddy')
-cool_reviewer.courses_attached += ['Python']
+cool_reviewer.courses_attached += ['Python', 'Git', 'Java']
 
 cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Git', 9)
+cool_reviewer.rate_hw(best_student, 'Java', 9)
+
+cool_reviewer.rate_hw(bad_student, 'Java', 3)
+cool_reviewer.rate_hw(bad_student, 'Git', 5)
+
+bad_reviewer = Reviewer('Big', 'Mama')
+bad_reviewer.courses_attached += ['Python', 'Java']
+
+bad_reviewer.rate_hw(best_student, 'Python', 8)
+bad_reviewer.rate_hw(best_student, 'Java', 7)
+
+bad_reviewer.rate_hw(bad_student, 'Java', 7)
 
 cool_lecturer = Lecturer('Homer', 'Simpson')
-cool_lecturer.courses_attached += ['Python']
+cool_lecturer.courses_attached += ['Python', 'Git', 'Java']
 
 best_student.rate_lecture(cool_lecturer, 'Python', 7)
-best_student.rate_lecture(cool_lecturer, 'Python', 7)
-best_student.rate_lecture(cool_lecturer, 'Python', 4)
+best_student.rate_lecture(cool_lecturer, 'Git', 7)
+best_student.rate_lecture(cool_lecturer, 'Java', 8)
+
+bad_student.rate_lecture(cool_lecturer, 'Git', 7)
+bad_student.rate_lecture(cool_lecturer, 'Java', 5)
+
+
+bad_lecturer = Lecturer('Marge', 'Simpson')
+bad_lecturer.courses_attached += ['Python', 'Java']
+
+best_student.rate_lecture(bad_lecturer, 'Python', 5)
+best_student.rate_lecture(bad_lecturer, 'Java', 3)
+
+bad_student.rate_lecture(bad_lecturer, 'Java', 8)
+
+all_students = [best_student, bad_student]
+def avg_grade_students(all_students, course):
+    all_grade = []
+    if all_students:
+        for students in all_students:
+            for key, value in students.grades.items():
+                if key == course:
+                    all_grade += value
+                return sum(all_grade) / len(all_grade)
+
+all_lecture = [cool_lecturer, bad_lecturer]
+def avg_grade_lecture(all_lecture, course):
+    all_grade = []
+    if all_lecture:
+        for lectures in all_lecture:
+            for key, value in lectures.lecture_grade.items():
+                if key == course:
+                    all_grade += value
+                return sum(all_grade) / len(all_grade)
 
 print(best_student.grades)
+print(bad_student.grades)
 print(cool_lecturer.lecture_grade)
+print(bad_lecturer.lecture_grade)
+print(best_student)
+print(bad_student)
+print(cool_lecturer)
+print(bad_lecturer)
+print(cool_reviewer)
+print(bad_reviewer)
+print(best_student < bad_student)
+print(cool_lecturer > bad_lecturer)
+print(avg_grade_students(all_students, 'Python'))
+print(avg_grade_lecture(all_lecture, 'Python'))
